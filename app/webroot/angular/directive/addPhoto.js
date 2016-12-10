ce15fd04c7c2dcce81c24aefcb953a570694d693
avner ainouz <aainouz@univ.haifa.ac.il>
2016-12-10 22:16:02 +0200

    work on gallery

---
+++ app/webroot/angular/directive/addPhoto.js
@@ -0,0 +1,29 @@
+(function(){
+	var rout = angular.module('app');
+	
+        rout.directive('modalr',modal);
+        
+        function modal() {
+        return {
+            templateUrl: 'partials/registration.html',
+            restrict: 'E',
+            transclude: true,
+            replace:true,
+
+            link:function link(scope, element, attrs){
+                
+                scope.$watch('showRegister', function(){
+//                      console.log('showRegister:',scope.showRegister);
+                    if(scope.showRegister === true){
+                   
+                        $(element).modal('show');
+                    }else{
+        
+                        $(element).modal('hide');
+                    }
+                });                
+            }
+        };
+     };
+	
+})();

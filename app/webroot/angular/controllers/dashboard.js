9ff4e4c1593162f3c4f81ba9ddb7299d176223a7
avner ainouz <aainouz@univ.haifa.ac.il>
2016-12-20 21:34:31 +0200

    finish statistic in user dashboard

--- app/webroot/angular/controllers/dashboard.js
+++ app/webroot/angular/controllers/dashboard.js
@@ -17,11 +17,14 @@
         $scope.deletion = 0;
         $scope.photos = 0;
         $scope.current_page = 1;
-//        $scope.commitsData = [];
+        $scope.commitsData = [];
+        $scope.commitsTime = [];
 
         var addition = [];
         var deletion = [];
         var categories = [];
+
+
         $scope.init = function () {
             $scope.current_page = 1;
             $scope.git_user = "";
@@ -31,9 +34,11 @@
             $scope.deletion = 0;
             $scope.photos = 0;
             $scope.commitsData = [];
+            $scope.commitsTime = [];
             addition = [];
             deletion = [];
             categories = [];
+
             userData = $.param({
                 id: $scope.$parent.infoData.id
             });
@@ -69,18 +74,15 @@
                                             }
                                             $scope.addition = numberWithCommas(sumAdd);
                                             $scope.deletion = numberWithCommas(sumDel);
-                                        }
+                                        
                                         init_git_chart();
+                                        }
                                     });
                             $http.get("https://api.github.com/repos/" + $scope.gituser + "/" + $scope.gitproject + "/commits?page=1&per_page=10")
                                     .then(function (response) {
 
-
-                                        console.log(response);
-//
                                         if (response.data.length > 0)
                                         {
-
                                             var data = response.data;
                                             for (var key in data) {
                                                 var commit = {
@@ -90,51 +92,37 @@
                                                 };
                                                 $scope.commitsData.push(commit);
                                             }
-                                            var counts = _.countBy($scope.commitsData, 'date');
 
-                                            console.log(counts);
-//                                            $scope.addition = numberWithCommas(sumAdd);
-//                                            $scope.deletion = numberWithCommas(sumDel);
                                         }
-//                                        init_git_chart();
                                     });
-                            var commitsGraph = [];        
-                            var next_url = "https://api.github.com/repos/" + $scope.gituser + "/" + $scope.gitproject + "/commits?per_page=50"
-                     
-                            $http.get(next_url)
+                            var rawCommits = "";
+                            var editCommits = [];
+
+                            $http.get("https://api.github.com/repos/" + $scope.gituser + "/" + $scope.gitproject + "/commits?per_page=100")
                                     .then(function (response) {
+                                        if (response.data.length > 0)
+                                        {
+                                            rawCommits = response.data;
+                                            for (var key in rawCommits) {
+                                                var commit = {
+                                                    date: extractDate(rawCommits[key].commit.committer.date)
+                                                };
+                                                editCommits.push(commit);
+                                            }
+//                    
+
+                                            var tempList = _.countBy(editCommits, 'date');
+
+
+                                            for (var key in tempList) {
+                                                var tDate = key.split("-");
+
+                                                $scope.commitsTime.push([Date.UTC(tDate[2], parseInt(tDate[1]) - 1, tDate[0]), tempList[key]]);
+                                            }
                                         
-                                        commitsGraph = response.data;
-//                                            for (var key in data) {
-//                                                var commit = {
-//                                                    date: extractDate(data[key].commit.committer.date),   
-//                                                };
-//                                                $scope.commitsGraph.push(commit);
-//                                            }
-//                                            console.log($scope.commitsGraph);
-                                        if (response.headers('link')){
-                                        var link = parse_link_header(response.headers('link'));
-                                        if (link.next){
-                                            next_url = link.next;
-                                             $http.get(next_url)
-                                             .then(function (response) {
-                                        
-                                               commitsGraph.push(response.data);
-                                                console.log(commitsGraph);
-                                             });
+                                        init_commits_chart();
                                         }
-                                    }
-                                
-                            });
-                       
-
-                                   
-//                                        }
-
-//                            response = requests.get(next_url)
-
-//                            
-
+                                    });
 
                         }
 
@@ -145,11 +133,7 @@
                                 "<hr />headers: " + header +
                                 "<hr />config: " + config;
                     });
-//
-//            $http.get("http://www.omdbapi.com/?s=" + $scope.search)
-//                    .then(function (response) {
-//                        $scope.related = response.data;
-//                    });
+
         };
         $scope.next = function () {
             $scope.current_page++;
@@ -194,11 +178,7 @@
 
                     });
         };
-//    https://api.github.com/repos/godav/final-projects-manager/commits
-//    
-//    
-
-
+   
         function init_git_chart()
         {
 
@@ -254,101 +234,67 @@
         }
 
 
-
-//        Highcharts.chart('commit-chart', {
-//            chart: {
-//                zoomType: 'x'
-//            },
-//            title: {
-//                text: 'התקדמות ה - COMMIT לאורך הפרויקט'
-//            },
-//            subtitle: {
-//                text: 'הקלק ומשוך לזום אין'
-//            },
-//            xAxis: {
-//                type: 'datetime'
-//            },
-//            yAxis: {
-//                title: {
-//                    text: 'Commits' + 'מספר '
-//                }
-//            },
-//            legend: {
-//                enabled: false
-//            },
-//            plotOptions: {
-//                area: {
-//                    fillColor: {
-//                        linearGradient: {
-//                            x1: 0,
-//                            y1: 0,
-//                            x2: 0,
-//                            y2: 1
-//                        },
-//                        stops: [
-//                            [0, Highcharts.getOptions().colors[0]],
-//                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-//                        ]
-//                    },
-//                    marker: {
-//                        radius: 2
-//                    },
-//                    lineWidth: 1,
-//                    states: {
-//                        hover: {
-//                            lineWidth: 1
-//                        }
-//                    },
-//                    threshold: null
-//                }
-//            },
-//
-//            series: [{
-//                type: 'area',
-//                name: 'Commits',
-//                data: data
-//            }]
-//        });
-
-
-
-
-        Highcharts.chart('commit-chart', {
-            title: {
-                text: 'התקדמות ה - COMMIT לאורך הפרויקט',
-                x: -20 //center
-            },
-            xAxis: {
-                title: {
-                    text: 'שבוע בפרויקט'
+        function init_commits_chart() {
+            Highcharts.chart('commit-chart', {
+                chart: {
+                    zoomType: 'x'
                 },
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            yAxis: {
                 title: {
-                    text: 'שורות קוד'
+                    text: 'האחרונים בפרויקט' + ' Commits ' + '-' + 'התפלגות 100 ה'
                 },
-                plotLines: [{
-                        value: 0,
-                        width: 1,
-                        color: '#808080'
+                subtitle: {
+                    text: 'הקלק ומשוך לזום אין'
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: 'Commits' + 'מספר '
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    area: {
+                        fillColor: {
+                            linearGradient: {
+                                x1: 0,
+                                y1: 0,
+                                x2: 0,
+                                y2: 1
+                            },
+                            stops: [
+                                [0, Highcharts.getOptions().colors[0]],
+                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                            ]
+                        },
+                        marker: {
+                            radius: 2
+                        },
+                        lineWidth: 1,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        },
+                        threshold: null,
+                        series: {
+                            connectNulls: true
+                        }
+                    }
+                },
+
+                series: [{
+                        type: 'area',
+                        name: 'Commits',
+                        data: $scope.commitsTime
                     }]
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: [{
-                    name: 'Tokyo',
-                    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                }, {
-                    name: 'New York',
-                    data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-                }]
-        });
+            });
+
+
+        }
 
         function numberWithCommas(x) {
             return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -370,103 +316,7 @@
             return hours + ":" + minutes;
         }
 
-
-        function parse_link_header(header) {
-            if (header.length === 0) {
-                throw new Error("input must not be of zero length");
-            }
-
-            // Split parts by comma
-            var parts = header.split(',');
-            var links = {};
-            // Parse each part into a named link
-            _.each(parts, function (p) {
-                var section = p.split(';');
-                if (section.length !== 2) {
-                    throw new Error("section could not be split on ';'");
-                }
-                var url = section[0].replace(/<(.*)>/, '$1').trim();
-                var name = section[1].replace(/rel="(.*)"/, '$1').trim();
-                links[name] = url;
-            });
-
-            return links;
-        }
-//        model.submit = function (isValid) {
-//
-//            if (isValid) {
-//                var photoData = null;
-//                if (model.photoName !== null && model.photoName !== "") {
-//                    var url = 'json/pages/pictureUpload';
-//                    var file = $scope.myFile;
-//                    var folder = 'users\\img\\project\\';
-//                    var id = $scope.$parent.infoData.id;
-//
-//                    fileService.uploadFileToUrl(file, url, folder, id, function (response) {
-//
-//                        if (response['full']) {
-//                            model.photoName = response['name'];
-//                            model.photoLocation = response['full'];
-//
-//                            photoData = $.param({
-//                                description: model.description,
-//                                title: model.title,
-//                                photo_name: model.photoName,
-//                                photo_location: model.photoLocation,
-//                                user_id: id
-//                            });
-//
-//                            var config = {
-//                                headers: {
-//                                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-//                                }
-//                            };
-//
-//                            $http.post('json/pages/addPhotoToUser', photoData, config)
-//                                    .success(function (data, status, headers, config) {
-//                                        if (data)
-//                                        {
-//
-//                                            model.photo.success = true;
-//                                            model.photo.message = "התמונה הועלתה!";
-//                                            $rootScope.$broadcast('galleryUpdated');
-////                                            $scope.$parent.updated = true;
-//                                            //    console.log($scope);
-//                                        }
-//
-//                                    })
-//                                    .error(function (data, status, header, config) {
-//                                        model.ResponseDetails = "Data: " + data +
-//                                                "<hr />status: " + status +
-//                                                "<hr />headers: " + header +
-//                                                "<hr />config: " + config;
-//                                    });
-//                        }
-//
-//
-//                    });
-//
-//                }
-//
-//            } else {
-//                model.message = "There are still invalid fields below";
-//            }
-//
-//        };
-
-
     }
 
 
 }());
-//
-
-
-
-//var d = new Date(1245398693390);
-//var formattedDate = d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
-//var hours = (d.getHours() < 10) ? "0" + d.getHours() : d.getHours();
-//var minutes = (d.getMinutes() < 10) ? "0" + d.getMinutes() : d.getMinutes();
-//var formattedTime = hours + ":" + minutes;
-//
-//formattedDate = formattedDate + " " + formattedTime;
